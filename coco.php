49379526164d63c46da0880cd67b377e6c18b24b
chairat1997 <jack702_@hotmail.com>
2022-03-03 15:00:39 +0700

    new commit

---
+++ coco.php
@@ -0,0 +1,190 @@
+<!doctype html>
+<html lang="en">
+  <head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="shortcut icon" type="image/x-icon" href="images/icons/coco.png" />
+    <title>ระบบจัดจำหน่ายผลิตภัณฑ์มะพร้าวจากสวน</title>
+
+<style>
+.f {
+    font-family: cursive;
+    font-size: 18px;
+    color: #128a43;
+    font-weight: bold;
+    font-style: normal;
+	text-align: center;
+}
+
+.image1:hover {
+  opacity: 0.3;
+}
+
+  .image1 {
+    display: block;
+    width: 65%;
+    height: 85%;
+    border-radius: 50%;
+    margin-left: 0%;
+}
+
+.navbar-light .navbar-nav .nav-link {
+    color: #87e88a;
+    margin: 0px 72px 0px 0px;
+}
+.navbar-light .navbar-nav .nav-link:focus, .navbar-light .navbar-nav .nav-link:hover {
+    color: #1bec5b;
+}
+
+.image:hover {
+  opacity: 0.3;
+ }
+
+.image {
+  display: block;
+  width: 40%;
+  height: 100%;
+  border-radius: 50%;
+}
+.i{
+	 margin-top: 3%;
+ }
+.bg-light {
+    background-color: #0a0c0ab8!important;
+}
+.re:link, .re:visited {
+    background-color: #3694f4;
+    color: white;
+    padding: 14px 18px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+	border-radius: 5px;
+	font-size: medium;
+	margin-top: 3%;
+	margin-right: 0%;
+}
+.re:hover, .re:active {
+    background-color: #175bf3;
+}
+
+.log:link, .log:visited {
+    background-color: #87e88a;
+    color: white;
+    padding: 14px 18px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+	border-radius: 5px;
+	font-size: medium;
+	margin-top: 3%;
+	margin-right: 0%;
+}
+.log:hover, .log:active {
+    background-color:#1bec5b;
+}
+.dropdown-item:hover {
+    color: #16181b;
+    text-decoration: none;
+    background-color: #75e5794a;
+}
+}
+@media only screen and (max-width: 768px) { /*แนวตั้ง*/
+.image1 {
+    display: block;
+    width: 65%;
+    height: 85%;
+    border-radius: 50%;
+    margin-left: 0px;
+}
+
+.i{
+	 margin-top: 5%;
+ }
+ .re:link, .re:visited {
+    background-color: #3694f4;
+    color: white;
+    padding: 12px 15px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+	border-radius: 5px;
+	font-size: medium;
+	margin-top: 15%;
+	margin-right: 3%;
+}
+
+.log:hover, .log:active {
+    background-color: #2e50cc;
+}
+
+.log:link, .log:visited {
+    background-color: #87e88a;
+    color: white;
+    padding: 12px 15px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+	border-radius: 5px;
+	font-size: medium;
+	margin-top: 15%;
+	margin-right: 3%;
+}
+
+.re:hover, .re:active {
+    background-color: #16ef1dd9;
+}
+}
+</style>
+  </head>
+  <body class="f">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="coco.php"><img src="icon.jpg" alt="logo" class="image1" title="ระบบจัดจำหน่ายผลิตภัณฑ์มะพร้าวจากสวน" ></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"				aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+	</button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+    
+    </ul>
+
+<form class="form-inline my-2 my-lg-0">
+	 <ul class="nav navbar-nav navbar-right">
+		<li class="nav-item dropdown">
+		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-sign-in"></i> เข้าสู่ระบบ</a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="login_user.php"><i class="fa fa-user-circle-o"></i> สมาชิก</a>
+          <a class="dropdown-item" href="register_user.php"><i class="fa fa-id-card"></i> สมัครสมาชิก</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="login_admin.php"><i class="fa fa-gear"></i> พนักงาน</a>
+        </div>
+      </li>
+	  </ul>
+</form>
+
+  </div>
+</nav>
+	
+	<div class="container i">
+		<div class="row"> <center><img src="homelogo.jpg" alt="logo" class="image">
+
+		</div>
+	</div>
+
+   <div class="container-fluid">
+		 <div class="row">
+			<div class="col-md-12 "><a href="login_user.php" class="log"><i class="fa fa-user-circle-o"></i> สมาชิก</a>  
+				<a href="register_user.php" class="re" ><i class="fa fa-id-card"></i> สมัครสมาชิก</a></div><br><br>
+
+		 </div>
+ 
+	</div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" crossorigin="anonymous"></script>
+  </body>
+</html>
